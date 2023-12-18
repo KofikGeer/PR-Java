5daabaf756c3ee5338c1ccbff4bde316306d4bbd
KofikGeer <147547900+KofikGeer@users.noreply.github.com>
2023-12-18 13:22:17 +0300

    Add files via upload

---
+++ PR_20/CalculatorView.java
@@ -0,0 +1,11 @@
+package PR_20;
+
+public class CalculatorView implements View {
+    public void printResult(String result) {
+        System.out.printf("%s\n", result);
+    }
+
+    public void printException(Exception e) {
+        System.out.printf("%s\n", e.toString());
+    }
+}
---
+++ PR_20/View.java
@@ -0,0 +1,7 @@
+package PR_20;
+
+public interface View {
+    void printResult(String result);
+
+    void printException(Exception e);
+}
